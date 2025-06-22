8a72984a837ee8d7c5e6c74a000067a1ffe90217
RyotoKuroki <kurohoge@gmail.com>
2025-06-22 16:13:16 +0900

    a

---
+++ app-mnt-host-aws/myapp-tailwind-components6/src/lib/prisma.ts
@@ -0,0 +1,23 @@
+// lib/prisma.ts
+
+import { PrismaClient } from '@prisma/client';
+
+// グローバルオブジェクトにPrismaClientインスタンスをキャッシュ
+declare global {
+  // eslint-disable-next-line no-var
+  var prisma: PrismaClient | undefined;
+}
+
+let prisma: PrismaClient;
+
+// 開発環境でのホットリロード時に新しいPrismaClientインスタンスが作成されないようにする
+if (process.env.NODE_ENV === 'production') {
+  prisma = new PrismaClient();
+} else {
+  if (!global.prisma) {
+    global.prisma = new PrismaClient();
+  }
+  prisma = global.prisma;
+}
+
+export default prisma;
