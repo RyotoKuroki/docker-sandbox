88e9ada13befe30e6fbeb449e7714def2669ec3c
rk <dwarf2wk@gmail.com>
2025-07-21 15:53:06 +0900

    Prettier導入

--- app-mnt-host-aws/myapp-tailwind-components6/src/app/(authorization-sample)/(un-authorized)/login/page.tsx
+++ app-mnt-host-aws/myapp-tailwind-components6/src/app/(authorization-sample)/(un-authorized)/login/page.tsx
@@ -1,50 +1,32 @@
-'use client'
+"use client";
 
-import {signIn} from "next-auth/react";
-
-
+import { signIn } from "next-auth/react";
 
 // ログイン画面 ボタンを押したらログイン成功画面へ遷移
 
 export default function SignInPage() {
+  const handleSignIn = () => {
+    // 第一引数は認証プロバイダ名 プロバイダによってリダイレクトなどの制御が異なるので指定が必要
 
+    signIn("credentials", {
+      // ログイン処理後のリダイレクト先
 
+      //callbackUrl: '/welcome',
+      callbackUrl: "/zod-authorized",
 
-    const handleSignIn = () => {
+      // 以降はクレデンシャル情報
 
-        // 第一引数は認証プロバイダ名 プロバイダによってリダイレクトなどの制御が異なるので指定が必要
+      username: "Snivy",
 
-        signIn('credentials', {
+      password: "strongPassword",
+    });
+  };
 
-            // ログイン処理後のリダイレクト先
-
-            //callbackUrl: '/welcome',
-            callbackUrl: '/zod-authorized',
-
-            // 以降はクレデンシャル情報
-
-            username: 'Snivy',
-
-            password: 'strongPassword',
-
-        });
-
-    };
-
-
-
-    return (
-
-        <div>
-
-            <button onClick={handleSignIn} className="border rounded p-3 m-3">
-
-                ログイン
-
-            </button>
-
-        </div>
-
-    );
-
+  return (
+    <div>
+      <button onClick={handleSignIn} className="border rounded p-3 m-3">
+        ログイン
+      </button>
+    </div>
+  );
 }
