a80555aa712bd7d1711788d5fca451b6e129816b
rk <dwarf2wk@gmail.com>
2025-07-27 05:02:01 +0900

    a

--- app-mnt-host-aws/myapp-tailwind-components6/src/lib/authOptions.ts
+++ app-mnt-host-aws/myapp-tailwind-components6/src/lib/authOptions.ts
@@ -1,117 +1,105 @@
 import CredentialsProvider from "next-auth/providers/credentials";
 
-import {NextAuthOptions} from "next-auth";
-
-
+import { NextAuthOptions } from "next-auth";
 
 // ログイン処理の設定を記述
 
 export const authOptions: NextAuthOptions = {
+  // プロバイダ
 
-    // プロバイダ
+  // ユーザの本人確認を責務とする
 
-    // ユーザの本人確認を責務とする
+  providers: [
+    // いわゆるID・パスワード方式
 
-    providers: [
+    // OAuthなども使えるが、今回はシンプルに扱えるクレデンシャル方式を採用
 
-        // いわゆるID・パスワード方式
+    CredentialsProvider({
+      // ログインのタイミングで呼ばれる関数
 
-        // OAuthなども使えるが、今回はシンプルに扱えるクレデンシャル方式を採用
+      // ここで画面の入力情報が特定のユーザと紐づくか、つまり認証の成否を判定
 
-        CredentialsProvider({
+      authorize(credentials, req) {
+        console.log(`called authOptions - authorize`);
 
-            // ログインのタイミングで呼ばれる関数
+        // (後述)
 
-            // ここで画面の入力情報が特定のユーザと紐づくか、つまり認証の成否を判定
+        // credentialsオブジェクトのプロパティは、signIn()で渡された引数と対応
 
-            authorize(credentials, req) {
+        if (!credentials?.username || !credentials?.password) {
+          console.log(`called authOptions - not found name/pass.`);
+          return null;
+        }
 
-                console.log(`called authOptions - authorize`);
+        const { username, password } = credentials;
 
-                // (後述)
+        // バックエンドなどと通信し、認証
+        console.log(`called authOptions - access db.`);
 
-                // credentialsオブジェクトのプロパティは、signIn()で渡された引数と対応
+        // ex:
 
-                if (!credentials?.username || !credentials?.password) {
+        // await login(username, password);
 
-                    console.log(`called authOptions - not found name/pass.`);
-                    return null;
+        return {
+          id: "USERID",
 
-                }
+          name: username,
+        };
+      },
 
-                const {username, password} = credentials;
+      // 本来は、このオプションからログイン画面を組み立てるときに参照されるプロパティ
 
+      // ただし、authorize関数の引数credentialsのキー名がここで定義したものに限定されるので、名前だけ設定しておく
 
-                // バックエンドなどと通信し、認証
-                console.log(`called authOptions - access db.`);
+      credentials: { username: {}, password: {} },
+    }),
+  ],
 
-                // ex:
+  // トークンをハッシュ化させたりなど、機密情報を生成するのに使うランダムな文字列
 
-                // await login(username, password);
+  secret: process.env.AUTH_SECRET,
 
+  session: {
+    strategy: "jwt",
+    //maxAge: 30 * 24 * 60 * 60,
+    maxAge: 60, //秒
+  },
+  // 特定のタイミングでNextAuth.jsから呼ばれる処理
 
+  callbacks: {
+    // JWTトークンを生成/更新する準備が整ったときに呼ばれる
 
-                return {
+    // トークンに何を含めるか
 
-                    id: "USERID",
+    async jwt({ token, user }) {
+      console.log(`called authOptions - jwt in callbacks.`);
 
-                    name: username
+      if (user?.name) {
+        console.log(`★Has User.name.`);
+        token.name = user.name;
+      }
 
-                }
+      console.log(`token -> ${JSON.stringify(token)}`);
+      return token;
+    },
 
-            },
+    // セッションがチェックされるたびに呼び出される
+    async session({ session, token }) {
+      console.log(`called authOptions - session in callbacks.`);
 
-            // 本来は、このオプションからログイン画面を組み立てるときに参照されるプロパティ
-
-            // ただし、authorize関数の引数credentialsのキー名がここで定義したものに限定されるので、名前だけ設定しておく
-
-            credentials: {username: {}, password: {}},
-
-        })
-
-    ],
-
-    // トークンをハッシュ化させたりなど、機密情報を生成するのに使うランダムな文字列
-
-    secret: process.env.AUTH_SECRET,
-
-    // 特定のタイミングでNextAuth.jsから呼ばれる処理
-
-    callbacks: {
-
-        // JWTトークンを生成/更新する準備が整ったときに呼ばれる
-
-        // トークンに何を含めるか
-
-        async jwt({token, user}) {
-            console.log(`called authOptions - jwt in callbacks.`);
-
-            if (user?.name) {
-
-                token.name = user.name
-
-            }
-
-            console.log(`token -> ${JSON.stringify(token)}`);
-            return token;
-
-        },
-
-        // セッションがチェックされるたびに呼び出される
-        async session({ session, token }) {
-            console.log(`called authOptions - session in callbacks.`);
-
-            // JWTトークンからセッションに情報を追加
-            if (token.name) {
-                //session.user.id = token.id as string;
-                if (!session!.user) {
-                    session.user = {};
-                }
-                session!.user!.name = token.name as string;
-            }
-            console.log(`session -> ${JSON.stringify(session)}`);
-            return session;
-        },
-    }
-
-}
+      // JWTトークンからセッションに情報を追加
+      if (token.name) {
+        //session.user.id = token.id as string;
+        if (!session!.user) {
+          session.user = {
+            role: null,
+            customeField: "追加したフィールド！！",
+          };
+        }
+        session!.user!.name = token.name as string;
+      }
+      console.log(`session -> ${JSON.stringify(session)}`);
+      return session;
+    },
+  },
+};
