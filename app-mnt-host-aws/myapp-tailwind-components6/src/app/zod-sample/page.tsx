a4d8be52097ae52c0f08775d3af21d0407c5da7f
rk <dwarf2wk@gmail.com>
2025-08-10 19:04:37 +0900

    a

--- app-mnt-host-aws/myapp-tailwind-components6/src/app/zod-sample/page.tsx
+++ app-mnt-host-aws/myapp-tailwind-components6/src/app/zod-sample/page.tsx
@@ -1,33 +1,31 @@
-'use client';
+"use client";
 
-import { useEffect, useMemo, useState/*, Suspense*/ } from 'react';
+import { useEffect, useMemo, useState /*, Suspense*/ } from "react";
 //import { standardSchemaResolver } from '@hookform/resolvers/standard-schema';
-import { useFieldArray, useForm } from 'react-hook-form';
-import { zodResolver } from '@hookform/resolvers/zod'; // これに変更
-import { z } from 'zod';
-import {
-  uiItemNames,
-  cautionName,
-} from "./schemas";
-import { Toaster, toast } from 'sonner';
-import { useSearchParams } from 'next/navigation';
-import MyModal from './dialog-sample';
-import CompletedArea from './completed-area';
+import { useFieldArray, useForm } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod"; // これに変更
+import { z } from "zod";
+import { uiItemNames, cautionName } from "./schemas";
+import { Toaster, toast } from "sonner";
+import { useSearchParams } from "next/navigation";
+import MyModal from "./dialog-sample";
+import CompletedArea from "./completed-area";
 //import Loading from "@/app/loading";
 import {
   inDtoSaveSomeSchema,
   inDtoSaveSomeType,
   inDtoSaveSomeErrorType,
+  ValidatiionSideEnum,
 } from "./server-actions/save-somedata/action-save-somedata-schema";
-import { sendMail } from './server-actions/action-sample';
+import { sendMail } from "./server-actions/action-sample";
 //import { ResultServ, validateOnServer } from './server-actions/action-validation';
-import Link from 'next/link';
-import { inDtoType } from './server-actions/initialize/action-initialize-schema';
-import { initializeAction } from './server-actions/initialize/action-initialize';
-import { ApiResultCommon } from '@/lib/api/ApiResultCommon';
+import Link from "next/link";
+import { inDtoType } from "./server-actions/initialize/action-initialize-schema";
+import { initializeAction } from "./server-actions/initialize/action-initialize";
+import { ApiResultCommon } from "@/lib/api/ApiResultCommon";
 import { compareAsc, format } from "date-fns";
-import { saveSomedataAction } from './server-actions/save-somedata/action-save-somedata';
-import { ChevronDownIcon, SlashIcon } from "lucide-react"
+import { saveSomedataAction } from "./server-actions/save-somedata/action-save-somedata";
+import { ChevronDownIcon, SlashIcon } from "lucide-react";
 import {
   Breadcrumb,
   BreadcrumbItem,
@@ -35,27 +33,25 @@ import {
   BreadcrumbList,
   BreadcrumbPage,
   BreadcrumbSeparator,
-} from "@/components/ui/breadcrumb"
+} from "@/components/ui/breadcrumb";
 import {
   DropdownMenu,
   DropdownMenuContent,
   DropdownMenuItem,
   DropdownMenuTrigger,
-} from "@/components/ui/dropdown-menu"
+} from "@/components/ui/dropdown-menu";
 
 /**
- * 
- * @returns 
+ *
+ * @returns
  */
 export default function HomePage() {
-  const searchParams = useSearchParams()
+  const searchParams = useSearchParams();
   // 初期化処理
   useEffect(() => {
-    
     // found search-param[p1]
-    const params1 = searchParams && searchParams.get('p1')
-    if (params1)
-      toast.info(`URLパラメータ：P1=${params1}`);
+    const params1 = searchParams && searchParams.get("p1");
+    if (params1) toast.info(`URLパラメータ：P1=${params1}`);
 
     // init in server
     const init = async () => {
@@ -63,7 +59,7 @@ export default function HomePage() {
         //currentDate: format(new Date(), "yyyy-MM-dd HH:mm:ss"),
         currentDate: new Date().toISOString(),
       } as inDtoType;
-      const resultInitAction = await initializeAction(inDto) as ApiResultCommon;
+      const resultInitAction = (await initializeAction(inDto)) as ApiResultCommon;
       if (!resultInitAction.success) {
         toast.error(`初期化失敗=${resultInitAction.errorMsg}`);
       } else {
@@ -71,7 +67,6 @@ export default function HomePage() {
       }
     };
     init();
-
   }, []);
 
   const [errors, setErrors] = useState<Partial<inDtoSaveSomeErrorType>>({});
@@ -89,19 +84,18 @@ export default function HomePage() {
         { priority: 1, subEmail: "sub1@somedomain.com" },
         { priority: 2, subEmail: "sub2@somedomain.com" },
         { priority: 3, subEmail: "sub3@somedomain.com" },
-      ]
+      ],
     },
-    mode: 'onBlur', // フォーカスが外れたときにバリデーションを実行
+    mode: "onBlur", // フォーカスが外れたときにバリデーションを実行
   });
   // useFieldArray を使用して、アイテムの配列を管理
   const { fields, append, remove } = useFieldArray({
-      control: proxyForm.control,
-      name: uiItemNames.emailList, // 'subs',
+    control: proxyForm.control,
+    name: uiItemNames.emailList, // 'subs',
   });
 
   /** submit処理 */
   const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
-    
     // ページリロードをキャンセル
     e.preventDefault();
 
@@ -109,15 +103,14 @@ export default function HomePage() {
     setErrors({});
     setSubmittedData(null);
     setIsMyModalOpened(false);
-    
+
     // フォームデータ取得
     const formData = proxyForm.getValues();
     let toastMsg = "";
     let resultData = null;
     // validation
     switch (formData.validSide) {
-
-      case "CLIENT":
+      case ValidatiionSideEnum.CLIENT:
         // validate on server-side
         const resultValidClient = inDtoSaveSomeSchema.safeParse(formData);
         if (!resultValidClient.success) {
@@ -125,18 +118,17 @@ export default function HomePage() {
           const fieldErrors = resultValidClient.error.flatten().fieldErrors as inDtoSaveSomeErrorType;
           setErrors(fieldErrors);
           setIsSubmitting(false);
-          
+
           toastMsg = "validation-error on【CLIENT】.";
         } else {
           resultData = resultValidClient.data;
         }
         break;
 
-      case "SERVER":
+      case ValidatiionSideEnum.SERVER:
         //const resultValidServer = await validateOnServer(formData) as ResultServ;
-        const resultSaveSome = await saveSomedataAction(formData) as ApiResultCommon;
-        if (!resultSaveSome.success &&
-             resultSaveSome.errorOnValidate) {
+        const resultSaveSome = (await saveSomedataAction(formData)) as ApiResultCommon;
+        if (!resultSaveSome.success && resultSaveSome.errorOnValidate) {
           const fieldErrors = resultSaveSome.errorOnValidate as inDtoSaveSomeErrorType;
           setErrors(fieldErrors);
           setIsSubmitting(false);
@@ -155,10 +147,10 @@ export default function HomePage() {
     if (toastMsg) {
       toast.error(
         <>
-          <div className='h-full w-full bg-green-300 bounded-xl p-3 m-3'>
+          <div className="h-full w-full bg-green-300 bounded-xl p-3 m-3">
             <h3>{toastMsg}</h3>
           </div>
-        </>
+        </>,
       );
       return;
     }
@@ -179,19 +171,17 @@ export default function HomePage() {
       // バリデーション成功
       // ここでAPIへのデータ送信などの非同期処理を行うことができます
       // 例: await new Promise(resolve => setTimeout(resolve, 1000));
-      console.log('Form submitted successfully:', resultData);
+      console.log("Form submitted successfully:", resultData);
       setSubmittedData(resultData);
       setIsMyModalOpened(true);
       // フォームをリセット（任意）
       // setFormData({ name: '', email: '' });
       setIsSubmitting(false);
-      
     }, 1100);
   };
 
   return (
     <div className="min-h-screen bg-gray-100 flex flex-col items-center justify-center p-4">
-
       <Breadcrumb>
         <BreadcrumbList>
           <BreadcrumbItem>
@@ -225,13 +215,9 @@ export default function HomePage() {
       </Breadcrumb>
 
       <div className="w-full max-w-md bg-white p-8 rounded-lg shadow-md">
-        <h1 className="text-2xl font-bold text-center text-gray-700 mb-6">
-          お問い合わせフォーム
-        </h1>
+        <h1 className="text-2xl font-bold text-center text-gray-700 mb-6">お問い合わせフォーム</h1>
 
-        <p className='whitespace-pre-wrap py-5'>
-          {cautionName}
-        </p>
+        <p className="whitespace-pre-wrap py-5">{cautionName}</p>
 
         <form onSubmit={handleSubmit} className="space-y-6">
           <div>
@@ -244,7 +230,7 @@ export default function HomePage() {
             <input
               type="text"
               className={`mt-1 block w-full px-3 py-2 border ${
-                errors.name ? 'border-red-500' : 'border-gray-300'
+                errors.name ? "border-red-500" : "border-gray-300"
               } rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm`}
               aria-describedby={`${uiItemNames.name}-${uiItemNames.error}`}
               {...proxyForm.register(uiItemNames.name)}
@@ -267,7 +253,7 @@ export default function HomePage() {
             <input
               type="email"
               className={`mt-1 block w-full px-3 py-2 border ${
-                errors.email ? 'border-red-500' : 'border-gray-300'
+                errors.email ? "border-red-500" : "border-gray-300"
               } rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm`}
               aria-describedby={`${uiItemNames.email}-${uiItemNames.error}`}
               {...proxyForm.register(uiItemNames.email)}
@@ -275,68 +261,63 @@ export default function HomePage() {
             />
             {errors.email && (
               <p id={`${uiItemNames.email}-${uiItemNames.error}`} className="mt-1 text-xs text-red-500">
-                {errors.email.join(', ')}
+                {errors.email.join(", ")}
               </p>
             )}
           </div>
 
           <div>
-            <label
-              className="block text-sm font-medium text-gray-700 mb-1"
-            >
-              ニックネーム
-            </label>
+            <label className="block text-sm font-medium text-gray-700 mb-1">ニックネーム</label>
             <input
               type="text"
               className={`mt-1 block w-full px-3 py-2 border ${
-                errors.nickname ? 'border-red-500' : 'border-gray-300'
+                errors.nickname ? "border-red-500" : "border-gray-300"
               } rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm`}
               {...proxyForm.register(uiItemNames.nickname)}
             />
-            {errors.nickname && (
-              <p className="mt-1 text-xs text-red-500">
-                {errors.nickname[0]}
-              </p>
-            )}
+            {errors.nickname && <p className="mt-1 text-xs text-red-500">{errors.nickname[0]}</p>}
           </div>
 
           <div>
-            <label
-              className="block text-sm font-medium text-gray-700 mb-1">
-              Subメールアドレス
-            </label>
+            <label className="block text-sm font-medium text-gray-700 mb-1">Subメールアドレス</label>
             {fields.map((sub, index) => (
-              <div className='flex flex-row justify-start items-start gap-2' key={index}>
-                <div className='w-[30%] items-start'>
+              <div className="flex flex-row justify-start items-start gap-2" key={index}>
+                <div className="w-[30%] items-start">
                   <input
                     type="number"
                     className={`mt-1 block w-full px-3 py-2 border ${
-                      (proxyForm.formState.errors.subs?.[index]?.priority) ? 'border-red-500' : 'border-gray-300'
+                      proxyForm.formState.errors.subs?.[index]?.priority ? "border-red-500" : "border-gray-300"
                     } rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm`}
                     aria-describedby={`${uiItemNames.emailList}-${index}-${uiItemNames.priority}-${uiItemNames.error}`}
                     {...proxyForm.register(`${uiItemNames.emailList}.${index}.${uiItemNames.priority}`)}
                     id={`${uiItemNames.emailList}.${index}.${uiItemNames.priority}`}
-                    placeholder='優先度'
+                    placeholder="優先度"
                   />
                   {proxyForm.formState.errors.subs?.[index]?.priority && (
-                    <p id={`${uiItemNames.emailList}-${index}-${uiItemNames.priority}-${uiItemNames.error}`} className="mt-1 text-xs text-red-500">
+                    <p
+                      id={`${uiItemNames.emailList}-${index}-${uiItemNames.priority}-${uiItemNames.error}`}
+                      className="mt-1 text-xs text-red-500"
+                    >
                       {proxyForm.formState.errors.subs[index]?.priority?.message}
                     </p>
                   )}
                 </div>
-                <div className='flex-1 items-start'>
+                <div className="flex-1 items-start">
                   <input
                     type="email"
                     className={`mt-1 block w-full px-3 py-2 border ${
-                      (proxyForm.formState.errors.subs?.[index]?.subEmail) ? 'border-red-500' : 'border-gray-300'
+                      proxyForm.formState.errors.subs?.[index]?.subEmail ? "border-red-500" : "border-gray-300"
                     } rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm`}
                     aria-describedby={`${uiItemNames.emailList}-${index}-${uiItemNames.subEmail}-${uiItemNames.error}`}
                     {...proxyForm.register(`${uiItemNames.emailList}.${index}.${uiItemNames.subEmail}`)}
                     id={`${uiItemNames.emailList}.${index}.${uiItemNames.subEmail}`}
-                    placeholder='sub-email'
+                    placeholder="sub-email"
                   />
                   {proxyForm.formState.errors.subs?.[index]?.subEmail && (
-                    <p id={`${uiItemNames.emailList}-${index}-${uiItemNames.subEmail}-${uiItemNames.error}`} className="mt-1 text-xs text-red-500">
+                    <p
+                      id={`${uiItemNames.emailList}-${index}-${uiItemNames.subEmail}-${uiItemNames.error}`}
+                      className="mt-1 text-xs text-red-500"
+                    >
                       {proxyForm.formState.errors.subs[index]?.subEmail?.message}
                     </p>
                   )}
@@ -345,15 +326,22 @@ export default function HomePage() {
             ))}
           </div>
 
-          <div className='flex flex-row gap-5'>
-            <div>
-              バリデーション処理
-            </div>
-            <div className='flex flex-row gap-3'>
-              <label><input type='radio' value="CLIENT"
-              {...proxyForm.register(uiItemNames.validSide)} defaultChecked /> Client</label>
-              <label><input type='radio' value="SERVER"
-              {...proxyForm.register(uiItemNames.validSide)} /> Server</label>
+          <div className="flex flex-row gap-5">
+            <div>バリデーション処理</div>
+            <div className="flex flex-row gap-3">
+              <label>
+                <input
+                  type="radio"
+                  value={ValidatiionSideEnum.CLIENT}
+                  {...proxyForm.register(uiItemNames.validSide)}
+                  defaultChecked
+                />{" "}
+                Client
+              </label>
+              <label>
+                <input type="radio" value={ValidatiionSideEnum.SERVER} {...proxyForm.register(uiItemNames.validSide)} />{" "}
+                Server
+              </label>
             </div>
           </div>
 
@@ -363,12 +351,14 @@ export default function HomePage() {
               disabled={isSubmitting}
               className="w-full flex justify-center py-2 px-4 border border-transparent rounded-md shadow-sm text-sm font-medium text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 disabled:bg-gray-400"
             >
-              {isSubmitting ? '送信中...' : '送信'}
+              {isSubmitting ? "送信中..." : "送信"}
             </button>
           </div>
-          
-          <div className='w-full text-center'>
-            <Link href="http://localhost:1080" target="_blank" className='text-blue-500'><u>maildev</u></Link>
+
+          <div className="w-full text-center">
+            <Link href="http://localhost:1080" target="_blank" className="text-blue-500">
+              <u>maildev</u>
+            </Link>
           </div>
         </form>
 
@@ -389,24 +379,24 @@ export default function HomePage() {
           <div className="flex flex-col items-center space-y-4 border-green-500 bg-green-200 p-5 rounded-2xl">
             {/* 歯車のように回るスピナー */}
             <div className="animate-spin h-22 w-22 border-4 border-blue-500 rounded-full border-t-transparent"></div>
-            <p className="text-blue-500 text-lg" aria-label="processing...">processing...</p>
+            <p className="text-blue-500 text-lg" aria-label="processing...">
+              processing...
+            </p>
           </div>
         </div>
       )}
 
       <footer className="mt-8 text-center text-gray-500 text-sm">
-        <p>
-          Powered by Next.js, Tailwind CSS, and Zod.
-        </p>
+        <p>Powered by Next.js, Tailwind CSS, and Zod.</p>
       </footer>
-      
-      {isMyModalOpened &&
+
+      {isMyModalOpened && (
         <MyModal
           open={isMyModalOpened}
-          children={<CompletedArea title='Succeeeeeeeeeeded!!' className='' />}
+          children={<CompletedArea title="Succeeeeeeeeeeded!!" className="" />}
           onCancel={() => setIsMyModalOpened(false)}
           onOk={() => {
-            setIsMyModalOpened(false)
+            setIsMyModalOpened(false);
 
             // toast
             toast.success("Succeeeeeeeeeeeded!");
@@ -416,7 +406,7 @@ export default function HomePage() {
                 <div className="flex flex-row space-x-2 items-center">
                   <div>Good Morning</div>
                 </div>
-                <div className='flex flex-row'>
+                <div className="flex flex-row">
                   <div>
                     <div className="flex justify-center items-center">
                       <div className="bg-yellow-400 rounded-full w-24 h-24 flex justify-center items-center">
@@ -437,20 +427,18 @@ export default function HomePage() {
                   </div>
                   <div>
                     <div className="flex flex-center justify-center items-center h-full w-full p-3">
-                      <h3>
-                        カスタムトーストだぜええええええ！！！
-                      </h3>
+                      <h3>カスタムトーストだぜええええええ！！！</h3>
                     </div>
                   </div>
                 </div>
-              </div>);
+              </div>,
+            );
           }}
         />
-      }
-    
-    {/** TODO 全画面で利用するならば、layout.tsx に記述した方が良い！ */}
-    <Toaster richColors />
+      )}
 
+      {/** TODO 全画面で利用するならば、layout.tsx に記述した方が良い！ */}
+      <Toaster richColors />
     </div>
   );
 }
--- app-mnt-host-aws/myapp-tailwind-components6/src/app/zod-sample/server-actions/save-somedata/action-save-somedata-schema.ts
+++ app-mnt-host-aws/myapp-tailwind-components6/src/app/zod-sample/server-actions/save-somedata/action-save-somedata-schema.ts
@@ -1,49 +1,48 @@
-import { z } from 'zod';
+import { z } from "zod";
+
+export enum ValidatiionSideEnum {
+  CLIENT = "CLIENT",
+  SERVER = "SERVER",
+}
 
 // schema of sub-email.
 const inDtoSubsSaveSomeSchema = z.object({
-    priority: z.coerce.number()
-          .min(1, { message: '1以上で入力してください' })
-          .max(100, { message: '100以下で入力してください' }),
-    subEmail: z.string()
-          .email({ message: '有効なメールアドレスを入力してください。' }),
+  priority: z.coerce
+    .number()
+    .min(1, { message: "1以上で入力してください" })
+    .max(100, { message: "100以下で入力してください" }),
+  subEmail: z.string().email({ message: "有効なメールアドレスを入力してください。" }),
 });
 // schema of this page form.
 const inDtoSaveSomeSchema = z.object({
-  name: z.string()
-          .nonempty({ message: '名前を入力してください！' })
-          .min(2, { message: '2文字以上で入力してください。' }),
-  email: z.string()
-          .email({ message: '有効なメールアドレスを入力してください。' }),
-  nickname: z.string()
-          .max(10, { message: '10以下で入力してください' })
-          .nullish(),
-  validSide: z.string(),
+  name: z
+    .string()
+    .nonempty({ message: "名前を入力してください！" })
+    .min(2, { message: "2文字以上で入力してください。" }),
+  email: z.string().email({ message: "有効なメールアドレスを入力してください。" }),
+  nickname: z.string().max(10, { message: "10以下で入力してください" }).nullish(),
+  validSide: z.union([z.literal(ValidatiionSideEnum.CLIENT), z.literal(ValidatiionSideEnum.SERVER)]),
   // age: z.number().min(18, { message: '18歳以上である必要があります。' }).optional(),
   subs: z.array(inDtoSubsSaveSomeSchema).min(1, {
-      message: '少なくとも1つのアイテムを追加してください',
+    message: "少なくとも1つのアイテムを追加してください",
   }),
 });
 // generate zod-object -> type
 type inDtoSaveSomeType = z.infer<typeof inDtoSaveSomeSchema>;
 // generate zod-object -> type of errors
-type inDtoSaveSomeErrorType = z.inferFlattenedErrors<typeof inDtoSaveSomeSchema>['fieldErrors'];
-
+type inDtoSaveSomeErrorType = z.inferFlattenedErrors<typeof inDtoSaveSomeSchema>["fieldErrors"];
 
 const outDtoSchema = z.object({
   msg: z.string(),
 });
 type outDtoType = z.infer<typeof outDtoSchema>;
-type outDtoErrorType = z.inferFlattenedErrors<typeof outDtoSchema>['fieldErrors'];
-
-
+type outDtoErrorType = z.inferFlattenedErrors<typeof outDtoSchema>["fieldErrors"];
 
 export {
-    inDtoSaveSomeSchema,
-    type inDtoSaveSomeType,
-    type inDtoSaveSomeErrorType,
-
-    outDtoSchema,
-    type outDtoType,
-    type outDtoErrorType,
-}
+  inDtoSaveSomeSchema,
+  type inDtoSaveSomeType,
+  type inDtoSaveSomeErrorType,
+  outDtoSchema,
+  type outDtoType,
+  type outDtoErrorType,
+};
