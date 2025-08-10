246ca01223a2f9911150dd9fb88d6d9576d8986d
rk <dwarf2wk@gmail.com>
2025-08-10 18:54:57 +0900

    a

--- app-mnt-host-aws/myapp-tailwind-components6/src/app/zod-sample/server-actions/save-somedata/action-save-somedata.ts
+++ app-mnt-host-aws/myapp-tailwind-components6/src/app/zod-sample/server-actions/save-somedata/action-save-somedata.ts
@@ -2,36 +2,34 @@
 
 import { ApiResultCommon } from "@/lib/api/ApiResultCommon";
 import {
-    inDtoSaveSomeSchema,
-    inDtoSaveSomeType,
-    inDtoSaveSomeErrorType,
-
-    outDtoSchema,
-    outDtoType,
-    outDtoErrorType,
+  inDtoSaveSomeSchema,
+  inDtoSaveSomeType,
+  inDtoSaveSomeErrorType,
+  outDtoSchema,
+  outDtoType,
+  outDtoErrorType,
 } from "./action-save-somedata-schema";
 import prisma from "@/lib/prisma";
 import { createUUID } from "@/lib/uuid-lib";
 import { format } from "date-fns";
 
 /**
- * @param inDto 
- * @returns 
+ * @param inDto
+ * @returns
  */
-export const saveSomedataAction = async (
-    inDto: inDtoSaveSomeType
-): Promise<ApiResultCommon> => {
+export const saveSomedataAction = async (inDto: inDtoSaveSomeType): Promise<ApiResultCommon> => {
+  console.log("### zod-sample backend called.");
 
-    try {
-        const resultValidation = inDtoSaveSomeSchema.safeParse(inDto);
-        if (!resultValidation.success) {
-            return {
-                success: false,
-                errorOnValidate: resultValidation.error.flatten().fieldErrors as inDtoSaveSomeErrorType
-            } as ApiResultCommon;
-        }
+  try {
+    const resultValidation = inDtoSaveSomeSchema.safeParse(inDto);
+    if (!resultValidation.success) {
+      return {
+        success: false,
+        errorOnValidate: resultValidation.error.flatten().fieldErrors as inDtoSaveSomeErrorType,
+      } as ApiResultCommon;
+    }
 
-        /*
+    /*
         prisma.$transaction(async (prisma) => {
 
             // insert log
@@ -66,21 +64,21 @@ export const saveSomedataAction = async (
         });
         */
 
-        console.log(`Success in serv.`);
-        const result = {
-            success: true,
-            data: {
-                msg: format(new Date(), "yyyy-MM-dd HH:mm:ss"),
-            } as outDtoType,
-        } as ApiResultCommon;
+    console.log(`Success in serv.`);
+    const result = {
+      success: true,
+      data: {
+        msg: format(new Date(), "yyyy-MM-dd HH:mm:ss"),
+      } as outDtoType,
+    } as ApiResultCommon;
 
-        return result;
-    } catch (ex: any) {
-        console.log(`Error in serv.`, ex.message);
+    return result;
+  } catch (ex: any) {
+    console.log(`Error in serv.`, ex.message);
 
-        return {
-            success: false,
-            errorMsg: JSON.stringify(ex.message),
-        } as ApiResultCommon;
-    }
-}
+    return {
+      success: false,
+      errorMsg: JSON.stringify(ex.message),
+    } as ApiResultCommon;
+  }
+};
