552b51407c274d08ddc52cd8432a4bc5c940cd5b
rk <dwarf2wk@gmail.com>
2025-08-10 18:28:27 +0900

    a

--- app-mnt-host-aws/myapp-tailwind-components6/src/app/components/breadcrumb/BreadcrumbCustom.tsx
+++ app-mnt-host-aws/myapp-tailwind-components6/src/app/components/breadcrumb/BreadcrumbCustom.tsx
@@ -16,7 +16,7 @@ import {
 } from "@radix-ui/react-dropdown-menu";
 import { ChevronDownIcon, SlashIcon } from "lucide-react";
 import Link from "next/link";
-import React, { forwardRef, useState } from "react";
+import React, { forwardRef, useEffect, useMemo, useState } from "react";
 import { IBreadCrumbLog } from "./interfaces/IBreadCrumbLog";
 
 interface BreadProps {
@@ -25,48 +25,79 @@ interface BreadProps {
 }
 const BreadcrumbCustom = forwardRef<HTMLDivElement, BreadProps>(
   ({ homeLabel, logs }: { homeLabel: string; logs: IBreadCrumbLog[] }, ref) => {
-    const [allLinks, setAllLinks] = useState([]);
+    //const [allLinks, setAllLinks] = useState<IBreadCrumbLog[]>([]);
+    //const [homeLink, setHomeLink] = useState("");
+    const [first, setFirst] = useState<IBreadCrumbLog | null>(null);
+    const [middleLogs, setMiddleLogs] = useState<IBreadCrumbLog[]>([]);
+    const [last, setLast] = useState<IBreadCrumbLog | null>(null);
 
-    const [homeLink, setHomeLink] = useState("");
-    const [innerLinks, setInnerLinks] = useState("");
-    const [latestLink, setLatestLink] = useState("");
+    useMemo(() => {
+      // 先頭
+      const first = logs != undefined && logs.length > 0 ? logs[0] : null;
+      setFirst(first);
+      // 末尾。ただしそもそも１件ならば先頭と同値なので無視する。
+      const last = logs != undefined && logs.length > 1 ? logs[logs.length - 1] : null;
+      setLast(last);
+      // ドロップダウンに隠すログリスト。ただしそもそも２件ならば先頭＋末尾と同値なので無視する。
+      debugger;
+      const mids = [] as IBreadCrumbLog[];
+      logs.forEach((each, index) => {
+        if (index == 0 || index == logs.length - 1) return;
+
+        const eachStr = JSON.stringify(each);
+        mids.push(JSON.parse(eachStr));
+      });
+      setMiddleLogs(mids);
+    }, [logs]);
 
     return (
       <div>
         <Breadcrumb>
           <BreadcrumbList>
-            {/* home */}
-            <BreadcrumbItem>
-              <BreadcrumbLink asChild>
-                <Link href="/bread-samples/page1">{homeLabel}</Link>
-              </BreadcrumbLink>
-            </BreadcrumbItem>
-            {/* divide */}
-            <BreadcrumbSeparator>
-              <SlashIcon />
-            </BreadcrumbSeparator>
-            {/* logs */}
-            <BreadcrumbItem>
-              <DropdownMenu>
-                <DropdownMenuTrigger className="flex items-center gap-1 [&_svg]:pointer-events-none [&_svg]:shrink-0 [&_svg:not([class*='size-'])]:size-3.5">
-                  logs
-                  <ChevronDownIcon />
-                </DropdownMenuTrigger>
-                <DropdownMenuContent align="start">
-                  <DropdownMenuItem>Documentation</DropdownMenuItem>
-                  <DropdownMenuItem>Themes</DropdownMenuItem>
-                  <DropdownMenuItem>GitHub</DropdownMenuItem>
-                </DropdownMenuContent>
-              </DropdownMenu>
-            </BreadcrumbItem>
-            {/* divide */}
-            <BreadcrumbSeparator>
-              <SlashIcon />
-            </BreadcrumbSeparator>
-            {/* LatestLink */}
-            <BreadcrumbItem>
-              <BreadcrumbPage>Breadcrumb</BreadcrumbPage>
-            </BreadcrumbItem>
+            {first != null && (
+              <>
+                <BreadcrumbItem>
+                  <BreadcrumbLink>
+                    <Link href={first.path}>{first.label}a</Link>
+                  </BreadcrumbLink>
+                </BreadcrumbItem>
+              </>
+            )}
+            {middleLogs != null && middleLogs.length > 0 && (
+              <>
+                <BreadcrumbSeparator>
+                  <SlashIcon />
+                </BreadcrumbSeparator>
+                <BreadcrumbItem>
+                  <DropdownMenu>
+                    <DropdownMenuTrigger className="flex items-center gap-1 [&_svg]:pointer-events-none [&_svg]:shrink-0 [&_svg:not([class*='size-'])]:size-3.5">
+                      logs
+                      <ChevronDownIcon />
+                    </DropdownMenuTrigger>
+                    <DropdownMenuContent align="start">
+                      {middleLogs.map((item) => (
+                        <DropdownMenuItem>{item.label}b</DropdownMenuItem>
+                      ))}
+                      {/* <DropdownMenuItem>Documentation</DropdownMenuItem>
+                        <DropdownMenuItem>Themes</DropdownMenuItem>
+                        <DropdownMenuItem>GitHub</DropdownMenuItem> */}
+                    </DropdownMenuContent>
+                  </DropdownMenu>
+                </BreadcrumbItem>
+              </>
+            )}
+            {last != null && (
+              <>
+                <BreadcrumbSeparator>
+                  <SlashIcon />
+                </BreadcrumbSeparator>
+                <BreadcrumbItem>
+                  <BreadcrumbLink>
+                    <Link href={last.path}>{last.label}c</Link>
+                  </BreadcrumbLink>
+                </BreadcrumbItem>
+              </>
+            )}
           </BreadcrumbList>
         </Breadcrumb>
       </div>
