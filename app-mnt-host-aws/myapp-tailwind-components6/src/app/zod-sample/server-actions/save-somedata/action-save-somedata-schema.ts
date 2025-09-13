f6df4609d1016783d2364daeb8af9f359e72c7ff
dwarf2wk <dwarf2wk@gmail.com>
2025-09-14 03:13:42 +0900

    a

--- app-mnt-host-aws/myapp-tailwind-components6/src/app/zod-sample/server-actions/save-somedata/action-save-somedata-schema.ts
+++ app-mnt-host-aws/myapp-tailwind-components6/src/app/zod-sample/server-actions/save-somedata/action-save-somedata-schema.ts
@@ -1,32 +1,49 @@
-import { z } from "zod";
+import { z, ZodString } from "zod";
 
 export enum ValidatiionSideEnum {
   CLIENT = "CLIENT",
   SERVER = "SERVER",
+  HOGE_1 = "HOGE_1",
+  HOGE_2 = "HOGE_2",
 }
+
+const validEmail = (zStr: ZodString): z.ZodString => {
+  return zStr.email({ message: "有効なメールアドレスを入力するっす。" });
+};
+
+const validNickName = (zStr: ZodString): z.ZodString => {
+  return zStr.nonempty({ message: "ひっす入力っす！" }).max(10, `[validNickName] 最大10桁っす`);
+};
+
+const validSubsPriority = (): z.ZodNumber => {
+  return z.coerce
+    .number()
+    .min(1, { message: "1以上で入力するっす" })
+    .max(100, { message: "100以下で入力するっす" });
+};
 
 // schema of sub-email.
 const inDtoSubsSaveSomeSchema = z.object({
-  priority: z.coerce
-    .number()
-    .min(1, { message: "1以上で入力してください" })
-    .max(100, { message: "100以下で入力してください" }),
+  priority: validSubsPriority(),
   subEmail: z.string().email({ message: "有効なメールアドレスを入力してください。" }),
 });
 // schema of this page form.
 const inDtoSaveSomeSchema = z.object({
-  name: z
-    .string()
-    .nonempty({ message: "名前を入力してください！" })
-    .min(2, { message: "2文字以上で入力してください。" }),
-  email: z.string().email({ message: "有効なメールアドレスを入力してください。" }),
-  nickname: z.string().max(10, { message: "10以下で入力してください" }).nullish(),
-  validSide: z.union([z.literal(ValidatiionSideEnum.CLIENT), z.literal(ValidatiionSideEnum.SERVER)]),
+  name: z.string().nonempty({ message: "名前を入力してください！" }),
+  email: validEmail(z.string()),
+  nickname: validNickName(z.string()),
+  validSide: z.union([
+    z.literal(ValidatiionSideEnum.CLIENT),
+    z.literal(ValidatiionSideEnum.SERVER),
+    z.literal(ValidatiionSideEnum.HOGE_1),
+    z.literal(ValidatiionSideEnum.HOGE_2),
+  ]),
   // age: z.number().min(18, { message: '18歳以上である必要があります。' }).optional(),
   subs: z.array(inDtoSubsSaveSomeSchema).min(1, {
     message: "少なくとも1つのアイテムを追加してください",
   }),
 });
+
 // generate zod-object -> type
 type inDtoSaveSomeType = z.infer<typeof inDtoSaveSomeSchema>;
 // generate zod-object -> type of errors
