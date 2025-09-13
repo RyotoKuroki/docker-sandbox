a35849cef164fbf9eed7589ed412dde0da02486a
dwarf2wk <dwarf2wk@gmail.com>
2025-09-14 03:17:58 +0900

    a

--- app-mnt-host-aws/myapp-tailwind-components6/src/app/page.tsx
+++ app-mnt-host-aws/myapp-tailwind-components6/src/app/page.tsx
@@ -4,7 +4,14 @@ export default function Home() {
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
       <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
-        <Image className="dark:invert" src="/next.svg" alt="Next.js logo" width={180} height={38} priority />
+        <Image
+          className="dark:invert"
+          src="/next.svg"
+          alt="Next.js logo"
+          width={180}
+          height={38}
+          priority
+        />
         <ol className="list-inside list-decimal text-sm/6 text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
           <li className="mb-2 tracking-[-.01em]">
             Get started by editing{" "}
@@ -23,7 +30,13 @@ export default function Home() {
             target="_blank"
             rel="noopener noreferrer"
           >
-            <Image className="dark:invert" src="/vercel.svg" alt="Vercel logomark" width={20} height={20} />
+            <Image
+              className="dark:invert"
+              src="/vercel.svg"
+              alt="Vercel logomark"
+              width={20}
+              height={20}
+            />
             Deploy now
           </a>
           <a
@@ -36,7 +49,10 @@ export default function Home() {
           </a>
         </div>
         <div className="w-full">
-          <a href="/zod-sample?p1=パラメ１" className="border-2 rounded-xl text-xl p-3 text-blue-700 w-full">
+          <a
+            href="/zod-sample?p1=パラメ１"
+            className="border-2 rounded-xl text-xl p-3 text-blue-700 w-full"
+          >
             Myサンプルページ &gt; &gt;
           </a>
         </div>
